5117192627759f4906348d4ad9bd03937e09197d
Amr Saleem <amr.2010100@gmail.com>
2024-02-08 18:55:29 +0200

    Add section in the main view to display the error
    -e.g: errors from fourms.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,18 @@
             </div>
         @endif
 
+    @if ($errors->any())
+        <div class="container">
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    @endif
+
         @yield('content')
     </main>
 
