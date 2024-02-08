3b2d9cd1d20fd0417b438d6271e18d3ccc510b10
Amr Saleem <amr.2010100@gmail.com>
2024-02-08 18:47:02 +0200

    Add comment policy.
    add comment plicy. so only comment owners can delete their comments.

---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Comment;
+
+class CommentPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function delete(User $user, Comment $comment)
+    {
+    return $user->id === $comment->user_id;
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 use App\Models\Post;
+use App\Models\Comment;
 use App\Policies\PostPolicy;
+use App\Policies\CommentPolicy;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -15,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Post::class => PostPolicy::class,
+        Comment::class => CommentPolicy::class,
 
     ];
 
